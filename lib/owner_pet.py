9d0ce0ea635b0eb4b56fa6a97381b2b5edafac3e
Stefan Klinkert <s.klinkert@gmail.com>
2024-08-19 19:23:35 +0200

    complete One to Many Lab

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,38 @@
+
 class Pet:
-    pass
+
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+
+    all = []
+
+    def __init__(self, name, pet_type, owner = None):
+        self.name = name
+        if pet_type in Pet.PET_TYPES:
+            self.pet_type = pet_type
+        else:
+            raise Exception("Pet ist not from the list")
+        self.owner = owner
+        Pet.all.append(self)
 
 class Owner:
-    pass
+
+    def __init__(self, name):
+        self.name = name
+    
+    def add_pet(self, pet):
+        if isinstance(pet,Pet):
+            pet.owner = self
+        else:
+            raise Exception("This pet is not ...")
+
+    # Pet needs to be method. e.g. for pet in Pet.all , do ...
+    def pets(self):
+        this_owners_pets = []
+        for cur_pet in Pet.all:
+            if cur_pet.owner == self:
+                this_owners_pets.append(cur_pet)
+        return this_owners_pets
+
+    def get_sorted_pets(self):
+        sorted_pets = sorted(self.pets(), key=lambda pet: pet.name)
+        return sorted_pets
